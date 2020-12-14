1b1822e71ea583a8b837a5f16c2a9b3e6ffa5ba0
Huy Vũ <huyvq18411c@st.uel.edu.vn>
2020-12-14 16:32:16 +0700

    commit changes

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@
 function showTemplate(contentHolder, url) {
   async function getTemplate() {
     var template = document.createElement("template");
-    template.innerHTML = await (await fetch(`./views/${url}`)).text();
+    template.innerHTML = await (await fetch(url)).text();
     var displayContent = template.content;
     contentHolder.appendChild(displayContent);
   }
