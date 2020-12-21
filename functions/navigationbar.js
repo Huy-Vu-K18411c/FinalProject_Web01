4f1e49d27990696596c7b23ad9dab5e162970946
Huy Vũ <vuquanghuy2k@gmail.com>
2020-12-22 01:22:40 +0700

    commit changes

--- functions/navigationbar.js
+++ functions/navigationbar.js
@@ -1,11 +1,19 @@
 "use strict";
 const app = angular.module("myApp", []);
 const navbar = app.component("navBar", {
-  templateUrl: "./views/navigationbar.html",
+  templateUrl: "./navigationbar.html",
 });
 
 const footer = app.component("footerWeb", {
-  templateUrl: "./views/footer.html",
+  templateUrl: "./footer.html",
+});
+
+const loginform = app.component("loginForm", {
+  templateUrl: "./login.html",
+});
+
+const signup = app.component("signUp", {
+  templateUrl: "./register.html",
 });
 
 angular.bootstrap(document.getElementById("headerHolder"), ["navBar"]);
