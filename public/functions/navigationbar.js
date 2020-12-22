abaea719566d89ecf077b3e9d8edd7152ea661a4
Huy Vũ <vuquanghuy2k@gmail.com>
2020-12-22 22:01:15 +0700

    commit changes

--- public/functions/navigationbar.js
+++ public/functions/navigationbar.js
@@ -24,4 +24,44 @@ const productDetails = app.component("productDetail", {
   templateUrl: "./productdetails.html",
 });
 
+app.controller("CollectionController", function ($scope) {
+  $scope.collections = [
+    {
+      name: "Accesories",
+      image:
+        "https://cdn.shopify.com/s/files/1/0276/4616/5110/collections/accessories_540x.jpg?v=1589428985",
+      quantity: 17,
+    },
+    {
+      name: "BackPacks",
+      image:
+        "https://cdn.shopify.com/s/files/1/0276/4616/5110/collections/bag_540x.jpg?v=1589429021",
+      quantity: 17,
+    },
+    {
+      name: "HomePage",
+      image:
+        "https://cdn.shopify.com/s/files/1/0276/4616/5110/collections/accessories_5e68ae61-8db3-4f19-a812-887b89310179_540x.jpg?v=1589814373",
+      quantity: 17,
+    },
+    {
+      name: "Shoes",
+      image:
+        "https://cdn.shopify.com/s/files/1/0276/4616/5110/collections/shoes_540x.jpg?v=1589429091",
+      quantity: 17,
+    },
+    {
+      name: "Women",
+      image:
+        "https://cdn.shopify.com/s/files/1/0276/4616/5110/collections/women_540x.jpg?v=1589429115",
+      quantity: 17,
+    },
+    {
+      name: "Men",
+      image:
+        "https://cdn.shopify.com/s/files/1/0276/4616/5110/collections/men_540x.jpg?v=1589429045",
+      quantity: 17,
+    },
+  ];
+});
 angular.bootstrap(document.getElementById("headerHolder"), ["navBar"]);
