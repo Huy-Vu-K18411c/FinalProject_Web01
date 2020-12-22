2d5cae0718a4c5a9036e7d940efdb33c3d8248df
Huy Vũ <vuquanghuy2k@gmail.com>
2020-12-22 23:15:03 +0700

    commit changes

--- public/functions/navigationbar.js
+++ public/functions/navigationbar.js
@@ -64,4 +64,71 @@ app.controller("CollectionController", function ($scope) {
     },
   ];
 });
+
+app.controller("BlogController", function ($scope) {
+  $scope.blogs = [
+    {
+      title: "Title 1",
+      date: "22/12/2012",
+      detail:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exofficia, hic quidem facere voluptates assumenda perspiciatis quodrepudiandae fugiat esse cumque, molestiae, quasi recusandae autrerum illo quas maiores error. Lorem ipsum dolor sit ametconsectetur, adipisicing elit...",
+      author: "Nhat Han",
+    },
+    {
+      title: "Title 2",
+      date: "21/12/2012",
+      detail:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exofficia, hic quidem facere voluptates assumenda perspiciatis quodrepudiandae fugiat esse cumque, molestiae, quasi recusandae autrerum illo quas maiores error. Lorem ipsum dolor sit ametconsectetur, adipisicing elit...",
+      author: "Quang Huy",
+    },
+    {
+      title: "Title 3",
+      date: "21/12/2012",
+      detail:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exofficia, hic quidem facere voluptates assumenda perspiciatis quodrepudiandae fugiat esse cumque, molestiae, quasi recusandae autrerum illo quas maiores error. Lorem ipsum dolor sit ametconsectetur, adipisicing elit...",
+      author: "Gian Don",
+    },
+    {
+      title: "Title 4",
+      date: "20/12/2012",
+      detail:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exofficia, hic quidem facere voluptates assumenda perspiciatis quodrepudiandae fugiat esse cumque, molestiae, quasi recusandae autrerum illo quas maiores error. Lorem ipsum dolor sit ametconsectetur, adipisicing elit...",
+      author: "Hong Oanh",
+    },
+    {
+      title: "Title 5",
+      date: "20/12/2012",
+      detail:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exofficia, hic quidem facere voluptates assumenda perspiciatis quodrepudiandae fugiat esse cumque, molestiae, quasi recusandae autrerum illo quas maiores error. Lorem ipsum dolor sit ametconsectetur, adipisicing elit...",
+      author: "Minh Thu",
+    },
+    {
+      title: "Title 6",
+      date: "19/12/2012",
+      detail:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exofficia, hic quidem facere voluptates assumenda perspiciatis quodrepudiandae fugiat esse cumque, molestiae, quasi recusandae autrerum illo quas maiores error. Lorem ipsum dolor sit ametconsectetur, adipisicing elit...",
+      author: "Nhat Han",
+    },
+  ];
+});
+
+function borderAnimation() {
+  document.getElementById("rect__border").style.strokeWidth = 1;
+}
+function stopAnimation() {
+  document.getElementById("rect__border").style.strokeWidth = 0;
+}
+
+app.controller("BlogItemController", function ($scope) {
+  $scope.blogs = [
+    {
+      title: "Anteposuerit litterarum formas.",
+      date: "22/12/2012",
+      detail:
+        "Diga, Koma and Torus are three kitchen utensils designed for Ommo, a new design-oriented brand introduced at the Ambiente show in February 2016. Minimalist approach, bright colors, stainless steel and matte plastic, abstract shapes and curved lines are the defining features of these products designed to be extremely functional, user-friendly and fun.Diga is a two-color melamine salad bowl where vegetables can be washed, drained and served. The disk at the bottom of the bowl can be turned counterclockwise to drain water when washing vegetables and it can be turned clockwise to lock the drain and hold condiments in the bowl when serving.Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Koma and Torus are two tea infusers, each with an original design and a concealed function. Koma has a round base and a long stainless steel-trimmed handle which offers a comfortable grip and allows.And the brushed steel cover opens and closes at the touch of a finger to easily fill and empty the infuser. The perfect way to enjoy brewing tea. Torus is donut-shaped and can cling to any cup. It is accompanied by a case that can contain up to three different diffusers and can be used for dry storage of loose tea.",
+      author: "Nhat Han",
+    },
+  ];
+});
+
 angular.bootstrap(document.getElementById("headerHolder"), ["navBar"]);
